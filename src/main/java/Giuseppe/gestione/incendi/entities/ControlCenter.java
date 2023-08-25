1e78e7fd863a536c5511fa6e6a6454089c6010c8
Peppe1795 <petrucci238@gmail.com>
2023-08-25 12:01:19 +0200

    creazione classi

---
+++ src/main/java/Giuseppe/gestione/incendi/entities/ControlCenter.java
@@ -0,0 +1,45 @@
+package Giuseppe.gestione.incendi.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import Giuseppe.gestione.incendi.interfaces.Observer;
+import Giuseppe.gestione.incendi.interfaces.Subject;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class ControlCenter implements Subject {
+
+	private UUID id;
+
+	private List<Sonda> sondeList = new ArrayList<>();
+
+	private ControlCenter(List<Sonda> sondeList) {
+		this.id = UUID.randomUUID();
+		this.sondeList = sondeList;
+	}
+
+	@Override
+	public void register(Observer o) {
+		sondeList.add((Sonda) o);
+
+	}
+
+	@Override
+	public void unregister(Observer o) {
+		sondeList.remove(o);
+
+	}
+
+	@Override
+	public void notifyObservers() {
+		for (Sonda s : sondeList) {
+			s.controlloLivelloFumo();
+		}
+
+	}
+
+}
--- src/main/java/Giuseppe/gestione/incendi/entities/Sonda.java
+++ src/main/java/Giuseppe/gestione/incendi/entities/Sonda.java
@@ -1,5 +1,40 @@
 package Giuseppe.gestione.incendi.entities;
 
-public class Sonda {
+import java.util.UUID;
+
+import Giuseppe.gestione.incendi.interfaces.Observer;
+import lombok.Data;
+
+@Data
+public class Sonda implements Observer {
+	private UUID id;
+	private String latitudine;
+	private String longitudine;
+	private Integer livelloFumo = 0;
+
+	private Sonda(String latitudine, String longitudine, Integer livelloFumo) {
+		this.id = UUID.randomUUID();
+		this.latitudine = latitudine;
+		this.longitudine = longitudine;
+		this.livelloFumo = livelloFumo;
+	}
+
+	@Override
+	public void controlloLivelloFumo() {
+		if (this.livelloFumo >= 5) {
+			String notificationUrl = generateNotificationUrl();
+			System.out.println("ALLARME!!! La sonda n. " + id + " ha registrato un livello di fumo di " + livelloFumo
+					+ ", latitudine: " + latitudine + " longitudine: " + longitudine);
+			System.out.println("\"Il centro di controllo ha mandato L'URL di notifica: " + notificationUrl);
+		} else {
+			System.out.println("Livello di fumo nei limiti!!");
+		}
+
+	}
+
+	public String generateNotificationUrl() {
+		return "http://host/alarm?idsonda=" + id + "&lat=" + latitudine + "&lon=" + longitudine + "&smokelevel="
+				+ livelloFumo;
+	}
 
 }
