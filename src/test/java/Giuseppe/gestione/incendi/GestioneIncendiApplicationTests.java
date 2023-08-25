c68059ab016f5ea626cc11300b8ffd29221a4b9d
Peppe1795 <petrucci238@gmail.com>
2023-08-25 10:31:13 +0200

    creazione progetto

---
+++ src/test/java/Giuseppe/gestione/incendi/GestioneIncendiApplicationTests.java
@@ -0,0 +1,13 @@
+package Giuseppe.gestione.incendi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneIncendiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
